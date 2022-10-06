eaef2bbef5b65428fe7c5446e6eea65d91a4ffb9
MartinFerreiraa <martinferreiraa4@gmail.com>
2022-10-06 20:22:27 -0300

    segunda entrega

--- app.js
+++ app.js
@@ -1,32 +1,26 @@
-const mostrarProductos = (data) => {
-    const contenedorProductos = document.getElementById('producto-contenedor')
-    data.forEach(producto => {
-        const div = document.createElement('div');
-        div.classList.add('card');
-        div.innerHTML = `<div class="card" style="width: 20rem;">
-                            <img src="${producto.img}" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">${producto.nombre}</h5>
-                                <p class="card-text">Categoria: ${producto.categoria}</p>
-                                <p class="card-text">Precio: $${producto.precio}</p>
-                                <p class="card-text">Stock: ${producto.stock}</p>                                
-                                <button class="btn btn-primary" id=boton${producto.id}>Comprar</button>
-                            </div>
-                        </div>`
-        contenedorProductos.appendChild(div)
+import { validarProductoRepetido } from "./src/accionesCarrito.js";
 
-        const boton = document.getElementById(`boton${producto.id}`);
-        boton.addEventListener('click', () => {
-            carritoIndex(producto.id)
-            alert(`Se agrego: 
-                ${producto.nombre}
-                Categoria:  ${producto.categoria}
-                Cantidad:  ${producto.cantidad}
-                Precio:   $${producto.precio}`)
-        })
+const mostrarProductos = (productos) => {
+  const contenedorProductos = document.getElementById("producto-contenedor");
 
+  productos.forEach(producto => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML += `<div class="card-content">
+                        <img src=${producto.img}>
+                        <span class="card-title">${producto.nombre}</span>
+                        <p>Precio: $ ${producto.precio}</p>
+                        <a class=" btn btn-warning" id=boton${producto.id}>Comprar</a>
+                        <p>Stock:${producto.stock}</p>`
+                                             
+                        
+    contenedorProductos.appendChild(div);
+
+    const boton = document.getElementById(`boton${producto.id}`);
+    boton.addEventListener('click', () => {
+      validarProductoRepetido(producto.id);
     })
+  });
+};
 
-}
-
-mostrarProductos(data)
+export { mostrarProductos };
--- src/stock.js
+++ src/stock.js
@@ -1,10 +1,11 @@
-const data = [
+const productos = [
     {
         id: 1,
         nombre: "Dolce & Gabbana The One",
         categoria: "Hombre",        
         precio: 5000,
         img: "https://d22fxaf9t8d39k.cloudfront.net/39a6d2eebaac7d3ec2db254fbbb32c3d672b13d624f1cc03df7e4bc3f9d9646d74448.png",
+        cantidad: 1,
         stock: 10
     },
     {
@@ -13,6 +14,7 @@ const data = [
         categoria: "Mujer",        
         precio: 6000,
         img: "https://www.perfumeriavip.com/21245-large_default/dg-the-one-edp-vapo30ml.jpg",
+        cantidad: 1,
         stock: 8
     },
     {
@@ -21,6 +23,7 @@ const data = [
         categoria: "Hombre",        
         precio: 5400,
         img: "https://cdn.shopify.com/s/files/1/2390/0819/products/ACH_Profumo_110ml_L7947100_3614270581670_RVB_580x.png?v=1630616727",
+        cantidad: 1,
         stock: 9
     },
     {
@@ -29,6 +32,7 @@ const data = [
         categoria: "Mujer",       
         precio: 3900,
         img: "https://cdn.shopify.com/s/files/1/2390/0819/products/ACH_EDT_Bottle_200ml_L5394101_3605521992703_RVB_580x.png?v=1630616707",
+        cantidad: 1,
         stock: 5
     },
     {
@@ -37,6 +41,7 @@ const data = [
         categoria: "Hombre",        
         precio: 7000,
         img: "https://www.rp-luxury.com/9788-medium_default/versace-eros-eau-de-parfum.jpg",
+        cantidad: 1,
         stock: 10
     },
     {
@@ -45,6 +50,8 @@ const data = [
         categoria: "Mujer",        
         precio: 6530,
         img: "https://cdnx.jumpseller.com/perfume-center-de-mexico/image/7562281/resize/570/765?1580067737",
+        cantidad: 1,
         stock: 7
     }
 ]
+export {productos};
